8e0f2c9ba1e30c493224e59721daa81873178eb0
Ayushmaan Gupta <146056029+ACUTE02@users.noreply.github.com>
2026-07-23 02:16:32 +0530

    update

--- work/work.py
+++ work/work.py
@@ -1,3 +1,5 @@
+import json
+
 from pydantic import BaseModel, Field, field_validator, model_validator,computed_field, ConfigDict, EmailStr, AnyUrl
 from typing import Optional, Literal
 from datetime import date
@@ -5,24 +7,28 @@ from datetime import date
 class Category(BaseModel):
     name:Literal["starter","main course","beverage","dessert","snacks"]
 
-class Category(BaseModel):
+class Model(BaseModel):
     model_config=ConfigDict(
-        extra='allow'
-        frozen=True
-        strict==True
+        extra="allow",
+        frozen=True,
+        strict=True,
         validate_assignment=True 
     )
     id             : int
     name           : str=Field(..., min_length=3, max_length=100, description="Item name")
     price          : int=Field(...,gt=0,description="Item Price")
-    category       : str=Field(...,description="Item Category")
-    is_available   : bool=Field(...,default=True)
+    category       : Category = Field(..., description="Item category")
+    is_available   : bool=Field(default=True)
     description    : Optional[str]=None
     added_by_email : EmailStr=Field(...,description="Email of the person")
     date_added     : date=Field(...,strict=False,)
 
+#Field Validator
+# @field_validator()
 
 
 
-item=work(id=1,name="panner tikka",price=120,category=Category(name='main course'),added_by_email=sdkl@gmail.com,date_added=12/1/2005)
+
+
+item=Model(id=1,name="panner tikka",price=120,category=Category(name='main course'),added_by_email="sdkl@gmail.com",date_added="2005-01-12")
 print(item)
